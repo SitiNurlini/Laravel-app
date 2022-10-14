a89f666a879d62c9f59254e96fc4876e314bb229
unknown <rezkiandra4123@gmail.com>
2022-10-14 15:27:31 +0700

    Revisi ke-6

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Categories extends Model
 {
@@ -12,4 +13,18 @@ class Categories extends Model
     protected $fillable = [
         'image', 'name', 'description'
     ];
+
+    public function products() {
+        return $this->hasMany(Products::class);
+    }
+
+    /**
+     * @return attribute
+     */
+
+    protected function image(): Attribute {
+        return Attribute::make(
+            get: fn ($value) => asset('/storage/categories/' . $value),
+        );
+    }
 }
